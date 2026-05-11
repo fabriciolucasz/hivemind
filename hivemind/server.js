28be981131e22277a839b88154d97c4f199640f9
Arthur_rdrg <rodrigues.arruda.arthur@gmail.com>
2026-05-11 11:43:27 -0300

    adicao do node no projeto

---
+++ hivemind/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/teste", (req, res) => {
+  res.json({
+    mensagem: "Backend funcionando!"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
